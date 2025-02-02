8ba5e7c29d3fa27e452f616392cf40eee901c3bb
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-02-02 11:48:54 -0500

    02 02 2025
    Excited to tackle this query down!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather Observation Station 18
+ Really interesting problem from HackerRank.com.  I have to calculate or query the "Manhattan Distance".
---
+++ Weather Observation Station 18.sql
@@ -0,0 +1,5 @@
+-------------------------02 02 2025-------------------------------------
+--this new problem
+--Weather Station 18 seems really interesting and hard
+
+--I am excited to tackle it!

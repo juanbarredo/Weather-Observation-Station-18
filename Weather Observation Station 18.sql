7658dd7ea1ff9f1cfd462bab2253aad2fb9eaead
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-02-03 20:26:04 -0500

    02 03 2025
    Super excited to be working on this problem using my new CERAKEY typewriter.
    
    This problem is very complex at the moment but I am excited to tackle it.

--- Weather Observation Station 18.sql
+++ Weather Observation Station 18.sql
@@ -3,3 +3,81 @@
 --Weather Station 18 seems really interesting and hard
 
 --I am excited to tackle it!
+
+-------------------------------------02 03 2025---------------------------------
+
+--today arrived my new keyboard.
+--the CERAKEY typewritter.
+
+--the keys are ceramic.
+
+--it sounds so good
+
+--it is also very much better ergonomically.
+
+
+--my fingers are glinding better
+--the numbers are easier to reach as well.
+
+--love it.
+
+--ok, problem Weather Observation Station 18.
+
+--my wrists are burning.
+
+
+--this ias a new keyboard.
+
+----ok,
+--one thing I just noticed.
+
+--this problem is labeled as "medium"
+--so let's calibrate to that.
+
+--it makes me feel good to work on a problem like this one because it 
+--feels very abstract.
+
+--I am definitely going to be challenged.
+
+--Consider p1(a,b) and p2(c,d)
+--to be two points on a 2d plane.
+
+--- a happens to equal the minimum value in Northern Latitude.
+--- (LAT_N in STATION).
+
+--- b happens to equal the minimum value in Western Longitude.
+--- (LONG_W in STATION).
+
+--- c happens to equal the maximum value in Northern Latitude.
+--- (LAT_N in STATION).
+
+-- d happens to be the maximum value in Western Longitude.
+--- (LONG_W in STATION).
+
+--Query the Manattan Distance 
+--between points p1 and p2
+--and round it to a scale of 4 decimal places.
+
+--wow, this problem is very, very interesting.
+
+--Hamming Distance: The distance between two points measured along axes at right angles.
+--In a plane with p1 at (x1,y1) and p2(x2,y2),
+--it is |x1 - x2| + |y1 - y2|.
+
+--ok, so here it laid out.
+
+--I currently have no idea how to go about working on this one
+--but I am very excited to figure it out!
+
+--SELECT list: I have to figure out how to do absolute.
+--list selections: i don't think there are many things that they are asking for list selections.  
+
+--So, next time I will look up how to do absolute.
+--
+--seems that the Syntax is ABS ( numeric_expression )
+
+--numeric_expression: an expression of the exact
+--numeric or appropriate numeric data type category.
+
+--doesn't look like it can do variables or column names at the moment.
+--
